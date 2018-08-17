313df682415a35a32be881a919d2192c7649b053
Kien Le <kien.le@etnetera.cz>
2018-08-17 09:42:17 +0200

    Created project

---
+++ Sources/ETPlaceholder.swift
@@ -0,0 +1,9 @@
+//
+//  ETPlaceholder.swift
+//  Etnetera a.s.
+//
+//  Created by Kien Le on 17/08/2018.
+//  Copyright © 2018 Etnetera a.s.. All rights reserved.
+//
+
+import Foundation
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import ETPlaceholderTests
+
+XCTMain([
+    testCase(ETPlaceholderTests.allTests),
+])
